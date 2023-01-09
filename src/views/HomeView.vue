8750977d0ad5c2119586a19edf7f3c2f487152b0
ILPONSE Bruno <bruno.ilponse@isae.fr>
2023-01-09 14:23:30 +0100

    Fix typos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,17 +15,16 @@
           mainly trying to reproduce games like Bomberman. As the 3D
           acceleration cards price started to fall in the 90s with the like of
           the Voodoo 3dfx I created some games using OpenGL and understood the
-          importance of maths at a practical level with matrix calculus in
-          particular. This led me to prepare entry for the French Engineering
-          schools after which I ended up with a degree in ...
-          Telecommunications. Indeed, it was all the rage at the time and I got
-          dragged into the hype.
+          importance of mathematicss at a practical level with matrix calculus
+          in particular. This led me to prepare entry for the French Engineering
+          schools providing me a degree in ... Telecommunications. Indeed, it
+          was all the rage at the time and I got dragged into the hype.
         </p>
         <p>
           Nevertheless, programming tracked me down, and after four years as a
           consultant in Telecommunications, I started developing professionally
-          in various entities such as consulting and
-          <b>software development companies</b>,
+          in various entities such as
+          <b>consulting and software development companies</b>,
           <b>educational institutions</b> and
           <b>decentralized autonomous organizations</b>.
         </p>
@@ -34,42 +33,43 @@
           <b>software development</b> activities and in many different
           situations, I feel like I have not even started scratching the surface
           of this field. Consequently, I do not consider myself an expert of any
-          particular language or framework. However I feel like I have tested my
-          <b>ability to adapt</b> to the limit.
+          particular language nor framework. However I feel like I have tested
+          my <b>ability to adapt</b> to the limit.
         </p>
       </div>
       <div class="one half animated bounceInRight triple-padded">
         <h1>Development languages</h1>
         <p>
-          Having worked in various companies, I had the opportunity to use many
+          Working in various companies gave me the opportunity to use many
           languages and frameworks.
         </p>
         <p>
-          I used <b>JAVA</b> in many contexts, first using JSP and servlets,
-          then <b>Spring</b> and <b>VertX frameworks</b>. I also deployed
-          applications on various application servers such as Tomcat and
+          I used the <b>JAVA</b> programming language in many contexts, first
+          using JSP and servlets, then <b>Spring</b> and
+          <b>VertX frameworks</b>. I also deployed applications on various
+          application servers such as <b>Tomcat</b>, <b>JBoss</b> and
+          <b>Jetty</b>
         </p>
         <p>
-          Having worked on CMSes and Frameworks like Drupal, Wordpress and
-          Laravel, I am also familiar with <b>PHP</b> and web server
-          configuration such as Apache and Nginx.
+          Using CMSes and Frameworks like Drupal, Wordpress and Laravel, I am
+          also familiar with <b>PHP</b> and web server configuration such as
+          Apache and Nginx.
         </p>
         <p>
-          I also developped modules for Redmine which had given me the
-          opportunity to dip my toes into <b>Ruby and Rails</b>.
+          I also developped modules for Redmine which allowed me to dip my toes
+          into <b>Ruby and Rails</b>.
         </p>
         <p>
           I created many applications and web-services using <b>Django</b> and
-          sometimes django-rest-framework for the backend and Vuejs for the
+          sometimes django-rest-framework for the backend and Vue.js for the
           frontend. Consequently, I acquired serious experience with
-          <b>Python</b> in general
+          <b>Python</b> in general.
         </p>
         <p>
-          In all my projects, I developed both the back-end and the front-end of
-          the applications, this enabled me to also progress in the various Web
-          technologies, ie. <b>HTML</b>, <b>CSS</b>, <b>Javascript</b>,
-          <b>Typescript</b>, <b>VueJS</b>, <b>Angular</b>, and lately
-          <b>React</b>.
+          In all my projects, I developed both the back-end and the front-end.
+          This enabled me to also progress in the various Web technologies, ie.
+          <b>HTML</b>, <b>CSS</b>, <b>Javascript</b>, <b>Typescript</b>,
+          <b>Vue.js</b>, <b>Angular</b>, and lately <b>React</b>.
         </p>
       </div>
     </div>
@@ -77,18 +77,18 @@
       <div class="one half animated bounceInLeft triple-padded">
         <h1>System administration</h1>
         <p>
-          I mostly worked in small teams. Thus, I had to acquire a set of
-          system's administration skills to 'get things done'.
+          I mostly worked in small teams. Thus, I had to acquire a set of system
+          administration skills to %3C%3Cget%20things%20done%3E%3E.
         </p>
         <p>
           In particular I feel confident with <b>Centos 7 & 8</b> system
           administration, application installation and configuration in general.
-          I also worked a little bit on Debian.
+          I also worked on some occasion on the <b>Debian</b> operating system.
         </p>
         <p>
           Particular applications I remember having configured include
           <b>Apache</b>, <b>Nginx</b>, as web-servers, reverse proxies or WSGI
-          servers but also <b>Tomcat</b>, <b>Jenkins</b>, <b>Sonarkube</b>.
+          servers but also <b>Tomcat</b>, <b>Jenkins</b>, <b>Sonarqube</b>.
         </p>
         <p>
           Moreover I worked on configuring the continuous integration systems on
@@ -97,7 +97,7 @@
         <p>
           Having installed and configured a small privately hosted
           <b>Kubernetes</b>
-          cluster in the context of a production grage microlearning
+          cluster in the context of a production grade microlearning
           application, I also feel confident working with that technology.
         </p>
       </div>
@@ -105,20 +105,19 @@
         <h1>Other skills</h1>
         <p>
           Since I worked in small companies and having a background in
-          consulting, I also had the opportunity to do the other jobs around
-          software developpers: Requirement analysis, Project management, Change
-          management.
+          consulting, I could develop other skills, in particular: Requirement
+          analysis, Project management and Change management.
         </p>
         <p>
           Most recently, as the technical lead of my latest project. I crafted a
           tailored <b>Agile</b> framework inspired from <b>Scrum</b> but adapted
-          to the very specific environnement we are working in.
+          to the very specific environnement the team is working in.
         </p>
       </div>
       <div class="one half animated bounceInRight triple-padded">
         <h1>Hobbies</h1>
         <p>
-          In no particular order: Roller hockey, Board games, Macro Economy,
+          In no specific order: Roller hockey, Board games, Macro economy,
           Blockchains, Video games.
         </p>
       </div>
